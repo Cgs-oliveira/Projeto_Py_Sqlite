fe97dc22ea95a255911546547fc8360c49f67cf8
Caio Oliveira <caio.gabriel@aluno.uece.br>
2024-04-15 23:18:51 -0300

    consultas
    as consultas foram feitas e configuradas por meio de funções(DEF)

--- criardb.py
+++ criardb.py
@@ -4,7 +4,7 @@ import sqlite3 as lite
 #criando conexão
 con = lite.connect('inventario_dados.db')
 
-#ciando tabelas|cur serve para apontar o banco de dados
+#criando tabelas|cur serve para apontar o banco de dados
 with con:
     cur=con.cursor()
     cur.execute("CREATE TABLE inventario(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, local TEXT, descricao TEXT, marca TEXT, data_da_compra DATE, valor_da_compra DECIMAL, serie TEXT, imagem TEXT)")
--- view.py
+++ view.py
@@ -5,30 +5,60 @@ import sqlite3 as lite
 con = lite.connect('inventario_dados.db')
 
 
-'''dados = ['sofa','sala de estar','magalu','ortobom','01/08/2024','1500','xxxxy','']'''
+dados = ['sofa','sala de estar','magalu','ortobom','01/08/2024','1500','xxxxy','F:\BANCO DE DADOS\Projeto_Py_Sqlite\img']
 
-# inserir dados
-
-with con:
-    cur=con.cursor() 
-    query= "DELETE FROM inventario where id= 3 "
-    cur.execute(query)
-
-'''ver_dados = []
-
-
+#CRUD
+#podemos usar o DB BROWSER para editar a tabela adicionar dados e remover tambem
+# CRIAR dados
+def inserir_from(i):
+    with con:
+        cur=con.cursor() 
+        query= "INSERT INTO inventario(nome, local, descricao, marca, data_da_compra,valor_da_compra,serie, imagem)VALUES(?,?,?,?,?,?,?,?)"
+        cur.execute(query,i)
 
 
-#ver dados da tabela
+#atualiza dados de inventario
+def atualizar(i):
+    with con:
+        cur=con.cursor() 
+        query= "UPDATE invetario SET nome=?, local=?, descricao=?, marca=?, data_da_compra=?,valor_da_compra=?,serie=?, imagem=? WHERE id="
+        cur.execute(query,i)
+    
 
-with con:
-    cur=con.cursor() 
-    query= "select * from inventario"
-    cur.execute(query)
+#podemos usar o DB BROWSER para editar a tabela adicionar dados e remover tambem
+# DELETAR dados
+def deletar_from(i):
+    with con:
+        cur=con.cursor() 
+        query= "DELETE FROM inventario where id=? "
+        cur.execute(query,i)
 
-    rows = cur.fetchall()
-    for row in rows:
-        ver_dados.append(row)
+#vizualizar tabela
+def ver_from(i):
+    ver_dados = []
+    with con:
+        cur=con.cursor() 
+        query= "select * from inventario"
+        cur.execute(query)
+
+        rows = cur.fetchall()
+        for row in rows:
+            ver_dados.append(row)
+    return (ver_dados)
+
+#ver dados separados
+def ver_item(id):
+    ver_item_individual = []
+    with con:
+            cur=con.cursor() 
+            query= "SELECT * from inventario WHERE id = ?"
+            cur.execute(query,id)
+
+            rows = cur.fetchall()
+            for row in rows:
+                ver_item_individual.append(row)
+        
 
 
-print (ver_dados)'''
+
+
